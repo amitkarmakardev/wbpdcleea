57d4140a92d7e0497b58fb946c7b9467803fb217
Amit Karmakar <a.karmakar@wbpdcl.co.in>
2016-09-29 17:10:00 +0530

    Create permissions in config file

--- config/wbpdcleea/permission.php
+++ config/wbpdcleea/permission.php
@@ -4,14 +4,78 @@ return [
 
     'admin' => [
 
+        'announcement' => [
+            'create',
+            'view',
+            'comment',
+        ],
+
+        'discussion' => [
+            'create',
+            'view',
+            'comment',
+        ],
+
+        'poll' => [
+            'create',
+            'view',
+            'vote',
+        ],
+
+        'member' => [
+            'create',
+            'update',
+        ],
+
+        'comment' => [
+            'view',
+            'moderate'
+        ],
     ],
 
     'moderator' =>[
 
+        'announcement' => [
+            'view',
+            'comment',
+        ],
+
+        'discussion' => [
+            'view',
+            'comment',
+        ],
+
+        'poll' => [
+            'view',
+            'vote',
+        ],
+
+        'comment' => [
+            'view',
+            'moderate'
+        ],
     ],
 
     'member' => [
 
+        'announcement' => [
+            'view',
+            'comment',
+        ],
+
+        'discussion' => [
+            'view',
+            'comment',
+        ],
+
+        'poll' => [
+            'view',
+            'vote',
+        ],
+
+        'comment' => [
+            'view',
+        ],
     ],
 
 ];
