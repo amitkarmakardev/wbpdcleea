7338312dd4a5b6572f8b1be5059606dbcfa1157d
Amit Karmakar <a.karmakar@wbpdcl.co.in>
2016-09-29 16:16:49 +0530

    Add js code to retain scroll position even after page reload

--- public/js/wbpdcleea.js
+++ public/js/wbpdcleea.js
@@ -2,17 +2,21 @@ $(window).on('load', function () {
     $("#spinner").fadeOut('slow');
 });
 
-// adding poll option
+// save current scroll position to session storage -> maintain scroll position after refresh
+$(window).scroll(function() {
+    sessionStorage.scrollTop = $(this).scrollTop();
+});
 
-function addPollOption(){
+// adding poll option
+function addPollOption() {
     $('#extra-poll-options').append(
-      '<div class="form-group"><label for="option">Option</label><input class="form-control" name="option[]" type="text"></div>'
+        '<div class="form-group"><label for="option">Option</label><input class="form-control" name="option[]" type="text"></div>'
     );
 }
 
 $(document).ready(function () {
 
-//    Create a confirmation dialog for links using bootbox.js
+    // create a confirmation dialog for links using bootbox.js
     $(document).on("click", "#confirm", function (e) {
         var link = $(this).attr("href"); // "get" the intended link in a var
         e.preventDefault();
@@ -23,8 +27,14 @@ $(document).ready(function () {
         });
     });
 
-    $(".nav a").on("click", function(){
+    $(".nav a").on("click", function () {
         // $(".nav").find(".active").removeClass("active");
         $(this).parent().addClass("active");
     });
+
+    // maintain scroll position even after refresh
+    if (sessionStorage.scrollTop != "undefined") {
+        $(window).scrollTop(sessionStorage.scrollTop);
+    }
+
 });
